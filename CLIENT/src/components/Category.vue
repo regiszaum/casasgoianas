78ace0c0de0f3ac67d9313cee8ebd8119610d234
Joao Mario Fidelis Martins <joao.martins@soluti.com.br>
2022-04-19 11:14:24 -0300

    Construção base da página Home

---
+++ CLIENT/src/components/Category.vue
@@ -0,0 +1,36 @@
+<template lang='pug'>
+  .category-card.d-flex.flex-column.text-start
+    .img-card
+        img(
+            :src='require(`@/assets/${imageName}`)'
+            alt='...'
+        )
+    span.ps-4.fw-bold {{productName}}
+</template>
+
+<script>
+
+export default {
+  props: {
+    categoryName: {
+      type: String,
+      required: true,
+    },
+    imageName: {
+      type: String,
+      required: true,
+    },
+  },
+};
+
+</script>
+
+<style scoped lang='stylus'>
+  .img-card
+    margin: 0 20px 7px 20px
+    overflow: hidden
+    width: 550px
+    height: 700px
+    border-radius: 7px
+    box-shadow: rgba(50, 50, 93, 0.25) 0px 6px 12px -2px, rgba(0, 0, 0, 0.3) 0px 3px 7px -3px;
+</style>
